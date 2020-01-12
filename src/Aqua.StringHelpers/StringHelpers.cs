5a13283d25c81262ea9c67856779bf0dc0c8bbe0
ualehosaini2019 <ualehosaini2019@hotmail.com>
2020-01-12 21:17:02 +0000

    GenerateLoremIpsumHtmlSafe has been added

--- src/Aqua.StringHelpers/StringHelpers.cs
+++ src/Aqua.StringHelpers/StringHelpers.cs
@@ -1109,5 +1109,48 @@ namespace Aqua.StringHelpers
             return s.Replace("'", "\"");
         }
 
+        /// <summary>
+        /// Generate Lorem Ipsum Html Paragraph text
+        /// </summary>
+        /// <param name="minWords"></param>
+        /// <param name="maxWords"></param>
+        /// <param name="minSentences"></param>
+        /// <param name="maxSentences"></param>
+        /// <param name="numParagraphs"></param>
+        /// <returns></returns>
+        public static string GenerateLoremIpsumHtmlSafe(int minWords, int maxWords,
+            int minSentences, int maxSentences,
+            int numParagraphs)
+        {
+
+            var words = new[]{"lorem", "ipsum", "dolor", "sit", "amet", "consectetuer",
+                                "adipiscing", "elit", "sed", "diam", "nonummy", "nibh", "euismod",
+                                "tincidunt", "ut", "laoreet", "dolore", "magna", "aliquam", "erat"};
+
+            Random rand = new Random();
+            int numSentences = rand.Next(maxSentences - minSentences) + minSentences + 1;
+
+            int numWords = rand.Next(maxWords - minWords) + minWords + 1;
+
+            StringBuilder result = new StringBuilder();
+
+            for (int p = 0; p < numParagraphs; p++)
+            {
+                result.Append("<p>");
+                for (int s = 0; s < numSentences; s++)
+                {
+                    for (int w = 0; w < numWords; w++)
+                    {
+                        if (w > 0) { result.Append(" "); }
+                        result.Append(words[rand.Next(words.Length)]);
+                    }
+                    result.Append(". ");
+                }
+                result.Append("</p>");
+            }
+
+            return result.ToString();
+        }
+
     }
 }
