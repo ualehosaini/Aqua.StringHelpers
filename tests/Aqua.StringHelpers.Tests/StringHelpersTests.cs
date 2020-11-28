68671c18a834d64e59fb0db3e66c6e2e4fcf23df
ualehosaini2019 <ualehosaini2019@hotmail.com>
2020-11-28 00:23:58 +0000

    Modernized test method

--- tests/Aqua.StringHelpers.Tests/StringHelpersTests.cs
+++ tests/Aqua.StringHelpers.Tests/StringHelpersTests.cs
@@ -72,10 +72,7 @@ namespace Aqua.StringHelpers.Tests
         [InlineData(null, null)]
         [InlineData("lorem     ipsum dolor    ", "lorem ipsum dolor")]
         [InlineData("  lorem     ipsum   dolor", "lorem ipsum dolor")]
-        public void RemoveExtraSpaces_Valid(string input, string expected)
-        {
-            Assert.Equal(expected, input.RemoveExtraSpaces());
-        }
+        public void RemoveExtraSpaces_Valid(string input, string expected) => Assert.Equal(expected, input.RemoveExtraSpaces());
 
         [Theory]
         [InlineData("", "")]
