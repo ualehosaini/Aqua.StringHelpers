caa4970fd430fef5325ea8150ca9e5d4da9a27a1
ualehosaini2019 <ualehosaini2019@hotmail.com>
2020-11-29 19:20:12 +0000

    A Test method has been modernized

--- tests/Aqua.StringHelpers.Tests/StringHelpersTests.cs
+++ tests/Aqua.StringHelpers.Tests/StringHelpersTests.cs
@@ -425,10 +425,7 @@ namespace Aqua.StringHelpers.Tests
         [InlineData(null, null)]
         [InlineData("", "")]
         [InlineData("qwertyuiop''[asdfghjkkll", "qwertyuiop\"\"[asdfghjkkll")]
-        public void ReplaceSingleQuotesWithDouble_Valid(string input, string expected)
-        {
-            Assert.Equal(expected, input.ReplaceSingleQuotesWithDouble());
-        }
+        public void ReplaceSingleQuotesWithDouble_Valid(string input, string expected) => Assert.Equal(expected, input.ReplaceSingleQuotesWithDouble());
 
         [Theory]
         [InlineData(null, 3, null)]
