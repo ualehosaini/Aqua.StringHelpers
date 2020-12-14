8fff8893f41082c66183e60508714bce2d93133d
ualehosaini2019 <ualehosaini2019@hotmail.com>
2020-12-14 19:11:46 +0000

    A test method has been enhanced for better readability

--- tests/Aqua.StringHelpers.Tests/StringHelpersTests.cs
+++ tests/Aqua.StringHelpers.Tests/StringHelpersTests.cs
@@ -26,7 +26,8 @@ namespace Aqua.StringHelpers.Tests
         [InlineData("abcDEF1234", true)]
         [InlineData("jdshj+03-98>22", false)]
         [InlineData("43434bdcd333 ", false)]
-        public void IsAlphaNumeric_Valid(string input, bool expected) => Assert.Equal(expected, input.IsAlphaNumeric());
+        public void IsAlphaNumeric_Valid(string input,
+                                         bool expected) => Assert.Equal(expected, input.IsAlphaNumeric());
 
         [Theory]
         [InlineData("", false)]
