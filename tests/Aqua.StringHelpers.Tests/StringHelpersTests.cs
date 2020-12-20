b9594a377d5e1d73574c636290721566c66d4113
ualehosaini2019 <ualehosaini2019@hotmail.com>
2020-12-20 15:14:15 +0000

    Test enhancements for better readability

--- tests/Aqua.StringHelpers.Tests/StringHelpersTests.cs
+++ tests/Aqua.StringHelpers.Tests/StringHelpersTests.cs
@@ -78,8 +78,9 @@ namespace Aqua.StringHelpers.Tests
         [Theory]
         [InlineData("ABC123", "ABC123")]
         [InlineData("12345+", "11+54321")]
-        public void Reverse_InValid(string input,
-                                    string expected) => Assert.NotEqual(expected, input.Reverse());
+        public void Reverse_InValid(
+            string input,
+            string expected) => Assert.NotEqual(expected, input.Reverse());
 
         [Theory]
         [InlineData("", "")]
